1c1516fcaa18b8cf930953e9ac9edcfe1d18d7af
DNepovim <jsem@dominikblaha.cz>
2016-12-02 10:59:55 +0100

    Show shortcodes in participants list

--- wp-content/mu-plugins/participants-manager/index.php
+++ wp-content/mu-plugins/participants-manager/index.php
@@ -19,3 +19,18 @@ require_once 'vintage_register.php';
 require_once 'vintage_settings.php';
 require_once 'participants-list.php';
 require_once 'register-form.php';
+
+! defined( 'ABSPATH' ) AND exit;
+/** Plugin Name: (#64933) »kaiser« Add post/page note */
+
+function wpse64933_add_posttype_note()
+{
+	global $post, $pagenow;
+
+	if ($pagenow == 'edit.php' && substr($post->post_type, 0, 5) == 'ptcm_') {
+		echo '<h2>Shortcodes</h2>';
+		echo '<p>Show register form: [register-form year=' . substr($post->post_type, 5) . ']</p>';
+		echo '<p>Show participants list: [participants-list year=' . substr($post->post_type, 5) . ']</p>';
+	}
+}
+add_action( 'all_admin_notices', 'wpse64933_add_posttype_note' );
